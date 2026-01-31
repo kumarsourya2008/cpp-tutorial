48d4ce90771f763a76f59327fd9dba11524ea156
SouryaKumar2008 <kumarsourya0803@gmail.com>
2026-01-31 23:43:32 +0530

    Added 'this->' pointer

---
+++ this_pointer.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Teacher
+{
+private:
+    double salary;
+
+public:
+    int ID;
+    string name;
+    string sub;
+    string dept;
+
+    // Parameterised constructor
+    Teacher(int ID, string name, string sub, string dept, double salary)  
+    // If both OBJECT properties and PARAMETERS of the "Teacher" constructor is same then we have to use "this->" pointer
+    {
+        this->ID = ID;
+        this->name = name;
+        this->sub = sub;
+        this->dept = dept;
+        this->salary = salary;
+    }
+
+    void getinfo(){
+        cout << "ID : " << ID << endl;
+        cout << "Name : " << name << endl;
+        cout << "Subject : " << sub << endl;
+        cout << "Department : " << dept << endl;
+        cout << "Salary : " << salary << endl;
+    }
+};
+
+int main()
+{
+    Teacher T1(2511, "Sourya Kumar", "C++", "Computer Science and Engineering", 50000);
+    T1.getinfo();
+
+    cout  << "--------------- X ---------------" << endl;
+
+    Teacher T2(2512, "Ram Kumar", "Math", "Computer Science and Engineering", 30000);
+    T2.getinfo();
+
+    return 0;
+}
