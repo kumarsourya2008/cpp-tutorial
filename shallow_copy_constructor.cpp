71e06838f13a7b06f4a734a5aec9928b7d78e7ef
SouryaKumar2008 <kumarsourya0803@gmail.com>
2026-02-01 18:52:29 +0530

    Added shalloe copy constructor

---
+++ shallow_copy_constructor.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Student{
+public:
+    string name;
+    double* cgpaPtr;
+
+    Student(string name, double cgpa){
+        this->name = name;
+        cgpaPtr = new double;
+        *cgpaPtr = cgpa;
+    }
+
+    Student(Student &obj){  // If we remove this custom copy constructor then the default constructor will give the same issue
+        this->name = obj.name;
+        this->cgpaPtr = obj.cgpaPtr;
+    }
+
+    void getInfo(){
+        cout << "Name : " << name << endl;
+        cout << "cgpa : " << *cgpaPtr << endl;
+    }
+};
+
+int main(){
+    Student S1("Sourya Kumar", 8.9);
+    Student S2(S1); // Rahul Kumar
+
+    S1.getInfo();
+    *(S2.cgpaPtr) = 9.2;
+    S1.getInfo();
+
+    return 0;
+}
