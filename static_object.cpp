973927add5eac0f25cbcfbf30b1adba210fa398d
SouryaKumar2008 <kumarsourya0803@gmail.com>
2026-02-05 22:00:15 +0530

    Added Static Object

---
+++ static_object.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class ABC{
+public:
+    ABC(){
+        cout << "Constructor" << endl;
+    }
+
+    ~ABC(){
+        cout << "Destructor" << endl;
+    }
+};
+
+int main(){
+    if (true)
+    {
+        static ABC obj;
+    }
+    cout << "End of main function" << endl;
+
+    return 0;
+}
